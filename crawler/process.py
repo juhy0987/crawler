51a5610f0d7d2ab2aceda4f407156f7abc283d05
Juhyun Kim <juhy0987@naver.com>
2024-04-26 16:36:47 +0900

    Make process only use the parameter in config(deepcopied)

--- crawler/process.py
+++ crawler/process.py
@@ -1,41 +1,71 @@
+import sys
 import time
 import atexit
 import threading
 import multiprocessing
 
-def processFailHanlder(q, curURL=("", 0)):
-  q.put(curURL)
+from .ThreadMgr import ThreadMgr
+from .ThreadMgr import thread
 
-def URLFailHandler(sURL):
-  # logging
-  pass
-
-def process (id, chiefMgrConn, config, q):
+def process (processId, chiefMgrConn, managers, urlQ, writerQ):
   if not chiefMgrConn:
     raise BaseException
   
-  # 0: main(writer) process, 1: judgement DB, 2: duplication check DB
-  lConn = [None, None, None]
+  configMgr = managers[0]
+  judgementTreeMgr = managers[1]
+  duplicationDBMgr = managers[2]
   
-  connUpdater = threading.Thread(name="process"+str(id)+"_judgeDBUpdater",
-                                        target=updater,
-                                        args=(lConn, chiefMgrConn))
   qEmptyTimeoutCnt = 0
-  if config.acquire(block=False):
-    qEmptyTimeoutLimit = config.qEmptyTimeoutLimit
-    config.release()
-  while qEmptyTimeoutCnt < qEmptyTimeoutLimit:
+  config = configMgr.getConfig()
+  threadMgr = ThreadMgr(config.MaxThread)
+  sys.stderr = open(config.LogFilePath, "at")
+  
+  # debug
+  # print("Process[{}] Initiated ".format(processId), file=sys.__stdout__)
+  updateCnt = 0
+  while qEmptyTimeoutCnt < config.qEmptyTimeoutLimit:
+    # config control
+    if chiefMgrConn.poll(0.1):
+      data = chiefMgrConn.recv()
+      try:
+        data = data.split()
+        match data[0]:
+          case "C":
+            match data[1]:
+              case "U":
+                config = configMgr.getConfig()
+                
+                # apply changed configuration
+                threadMgr.setMaxThread(config.MaxThread)
+              case _:
+                pass
+          case _:
+            pass
+      except Exception as e:
+        print("[Process Insite] Error occured: {}".format(str(e)), file=sys.__stdout__)
     
+    # debug
+    # print("Process[{}] {} Threads Running".format(processId, len(threadMgr.threads)), file=sys.__stdout__)
     
-    if q.empty():
+    deadlist = []
+    for id in threadMgr.threads.keys():
+      if not threadMgr.isThreadAlive(id):
+        deadlist.append(id)
+    for id in deadlist:
+      threadMgr.delThread(id)
+      threadMgr.setUnusedNum(id)
+    
+    if len(threadMgr.threads) < config.MaxThread and not urlQ.empty():
+      threadMgr.addThread(thread, (config, managers, urlQ, writerQ))
+    
+    if urlQ.empty() and not len(threadMgr.threads):
       qEmptyTimeoutCnt += 1
-      time.sleep(1)
     else:
       qEmptyTimeoutCnt = 0
+    
+    time.sleep(1)
+  
+  # print("Process[{}] Finally Terminated ".format(processId), file=sys.__stdout__)
+  
 
-def updater(conn):
-  with condition:
-    while True:
-      condition.wait()
-      
   
