6823ac559a5154a6e0de317be802044ff69f3499
Juhyun Kim <juhy0987@naver.com>
2024-05-03 09:19:09 +0900

    Add keyword query

--- query/oracQry.py
+++ query/oracQry.py
@@ -2,21 +2,33 @@
 
 ###################### query ########################
 # select
-selectDict = dict()
-selectDict["filter_setup_001"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT001' order by filter_string desc
+# 0: Normal URL token tree
+# 1: Regex URL token tree
+treeDict = dict()
+treeDict["filter_setup_001"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT001' order by filter_string desc
 """, 0)
 
-selectDict["filter_setup_002"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT002'
+treeDict["filter_setup_002"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT002'
 """, 0)
 
-selectDict["filter_setup_003"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT003'
+treeDict["filter_setup_003"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT003'
 """, 1)
 
-selectDict["filter_setup_004"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT004'
+treeDict["filter_setup_004"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WHERE filter_type='FT004'
 """, 1)
 
-selectDict["filter_white_url"] = ("""SELECT host_name || NVL(uri, '') FROM tbl_white_url
+treeDict["filter_white_url"] = ("""SELECT host_name || NVL(uri, '') FROM tbl_white_url
 """, 0)
+
+keywordQuery = """SELECT DISTINCT KEY_NO, WEIGHTED_VALUE, KEYWORD_HEX
+			FROM
+			(
+				SELECT B.KEY_NO, A.WEIGHTED_VALUE, B.KEYWORD_HEX
+				FROM TBL_KEYWORD_HEX B, TBL_KEYWORD A
+				WHERE A.KEY_NO = B.KEY_NO AND B.ENCODING_SET = 'ES001'
+			)
+			WHERE KEY_NO IN (SELECT KEY_NO FROM TBL_KEYWORD_SET WHERE KEY_GROUP_NO = {});
+"""
 
 # update
 
