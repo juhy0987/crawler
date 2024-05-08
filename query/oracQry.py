6e2c3c646e5e9cfb88b8f2cb9128290bd0757f84
Juhyun Kim <juhy0987@naver.com>
2024-05-08 13:50:39 +0900

    Match the form with other cases

--- query/oracQry.py
+++ query/oracQry.py
@@ -20,14 +20,15 @@ treeDict["filter_setup_004"] = ("""SELECT filter_string FROM TBL_FILTER_SETUP WH
 treeDict["filter_white_url"] = ("""SELECT host_name || NVL(uri, '') FROM tbl_white_url
 """, 0)
 
-keywordQuery = """SELECT DISTINCT KEY_NO, WEIGHTED_VALUE, KEYWORD_HEX
+keywordDict = dict()
+keywordDict["base"] = """SELECT DISTINCT KEY_NO, WEIGHTED_VALUE, KEYWORD_HEX
 			FROM
 			(
 				SELECT B.KEY_NO, A.WEIGHTED_VALUE, B.KEYWORD_HEX
 				FROM TBL_KEYWORD_HEX B, TBL_KEYWORD A
 				WHERE A.KEY_NO = B.KEY_NO AND B.ENCODING_SET = 'ES001'
 			)
-			WHERE KEY_NO IN (SELECT KEY_NO FROM TBL_KEYWORD_SET WHERE KEY_GROUP_NO = {});
+			WHERE KEY_NO IN (SELECT KEY_NO FROM TBL_KEYWORD_SET WHERE KEY_GROUP_NO = {})
 """
 
 # update
