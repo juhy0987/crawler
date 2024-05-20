9b9435b781f461156887b59e8c0b320451e06aa0
Juhyun Kim <juhy0987@naver.com>
2024-05-20 16:46:11 +0900

    add exception handling

--- modules/procSig.py
+++ modules/procSig.py
@@ -4,7 +4,10 @@ import signal
 import subprocess
 
 def killByPID(pid):
-  if sys.platform == 'win32':
-    p = subprocess.Popen(["taskkill", "/pid", str(pid), "/t", "/f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-  elif sys.platform == 'linux':
-    p = subprocess.Popen(["kill", "-9", str(pid)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+  try:
+    if sys.platform == 'win32':
+      p = subprocess.Popen(["taskkill", "/pid", str(pid), "/t", "/f"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    elif sys.platform == 'linux':
+      p = subprocess.Popen(["kill", "-9", str(pid)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+  except (KeyError, OSError):
+    pass
