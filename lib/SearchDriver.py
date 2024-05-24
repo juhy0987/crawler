faeb0d718b01057b550bd102f4ddf8e2e598ce4e
Juhyun Kim <juhy0987@naver.com>
2024-05-24 15:40:49 +0900

    Add: more evade bot detecting options

--- lib/SearchDriver.py
+++ lib/SearchDriver.py
@@ -8,9 +8,7 @@ from bs4 import BeautifulSoup
 lUserAgent = [
   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
-  "Mozilla/5.0 (Linux; Android 8.0.0; SAMSUNG-SM-G950N/KSU3CRJ1 Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/8.2 Chrome/63.0.3239.111 Safari/537.36",
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
-  "Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/71.0.3578.89 Safari/605.1",
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 ]
 nPos = 0
@@ -32,11 +30,14 @@ class SearchDriver(webdriver.Chrome):
     if sys.platform == 'linux':
       self.options.add_argument('no-sandbox') # Error in Windows
       self.options.add_argument('disable-dev-shm-usage')
-    
-    # humanlike options
     self.options.add_argument('disable-gpu')
     self.options.add_argument('lang=ko_KR')
+    
+    # humanlike options
     self.options.add_argument('user-agent='+getUA())
+    self.options.add_argument("disable-blink-features=AutomationControlled")
+    self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    self.options.add_experimental_option('useAutomationExtension', False)
     
     # delogging
     if sys.platform == 'win32':
@@ -63,6 +64,7 @@ class SearchDriver(webdriver.Chrome):
     ssl._create_default_https_context = ssl._create_unverified_context
     
     super().__init__(service=self.service, options=self.options)
+    self.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
   
   def __del__ (self):
     self.quit()
